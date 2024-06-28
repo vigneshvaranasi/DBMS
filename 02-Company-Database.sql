240e7ee739f151efab4dbd598ced278d65b75566
vigneshvaranasi <varanasivsv@gmail.com>
2024-06-28 16:02:41 +0530

    add company database

---
+++ 02-Company-Database.sql
@@ -0,0 +1,132 @@
+create database COMPANY;
+use COMPANY;
+show tables;
+-- Create Tables start
+create table EMPLOYEE(
+	Fname varchar(10) not null,
+	Minit char,
+	Lname varchar(20) not null,
+	Ssn char(9) not null,
+	Bdate date,
+	Address varchar(30),
+	Sex char(1),
+	Salary decimal(5),
+	Super_ssn char(9),
+	Dno int not null,
+	primary key (Ssn)
+);
+
+desc EMPLOYEE;
+
+create table DEPARTMENT(
+	Dname varchar(15) not null,
+	Dnumber int not null,
+	Mgr_ssn char(9) not null,
+	Mgr_start_date date,
+	PRIMARY KEY(Dnumber),
+	UNIQUE(Dname),
+	foreign key(Mgr_ssn) references EMPLOYEE(Ssn)
+);
+
+desc DEPARTMENT;
+
+create table DEPT_LOCATIONS(
+	Dnumber int not null,
+	Dlocation varchar(15) not null,
+	primary key (Dnumber,Dlocation),
+	foreign key (Dnumber) references DEPARTMENT(Dnumber)
+);
+
+desc DEPT_LOCATIONS;
+
+create table PROJECT(
+	Pname varchar(15) not null,
+    Pnumber int not null,
+    Plocation varchar(15),
+    Dnum int not null,
+    primary key (Pnumber),
+    unique (Pname),
+    foreign key (Dnum) references DEPARTMENT(Dnumber)    
+);
+
+desc PROJECT;
+
+create table WORKERS_ON(
+	Essn char(9) not null,
+    Pno int not null,
+    Hours decimal(3,1) not null,
+    primary key (Essn,Pno),
+    foreign key (Essn) references EMPLOYEE(Ssn),
+    foreign key (Pno) references PROJECT(Pnumber)    
+);
+
+desc WORKERS_ON;
+
+create table DEPENDENT(
+	Essn char(9) not null,
+    Dependent_name varchar(15) not null,
+    Sex char,
+    Bdate date,
+    Relationship varchar(8),
+    primary key (Essn,Dependent_name),
+    foreign key (Essn) references EMPLOYEE(Ssn)  
+);
+
+desc DEPENDENT;
+
+-- Create Tables end
+
+-- Alter tables start 
+
+alter table DEPARTMENT add constraint Dep_emp foreign key (Mgr_ssn) references EMPLOYEE(Ssn);
+desc DEPARTMENT;
+
+alter table EMPLOYEE add constraint Emp_emp foreign key (Super_ssn) references EMPLOYEE(Ssn);
+desc EMPLOYEE;
+
+alter table EMPLOYEE add constraint Emp_dno foreign key (Dno) references DEPARTMENT(Dnumber);
+desc EMPLOYEE;
+
+alter table EMPLOYEE add constraint Emp_super foreign key (Super_ssn) references EMPLOYEE(Ssn);
+desc EMPLOYEE;
+
+-- Alter tables end
+
+
+-- Insert Data start
+
+
+insert into EMPLOYEE
+values
+    ('John', 'B', 'Smith', 123456789, '1965-01-09', '731 Fondren, Houston TX', 'M', 30000, 333445555, 5),
+    ('Franklin', 'T', 'Wong', 333445555, '1965-12-08', '638 Voss, Houston TX', 'M', 40000, 888665555, 5),
+    ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321 Castle, Spring TX', 'F', 25000, 987654321, 4),
+    ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291 Berry, Bellaire TX', 'F', 43000, 888665555, 4),
+    ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975 Fire Oak, Humble TX', 'M', 38000, 333445555, 5),
+    ('Joyce', 'A', 'English', 453453453, '1972-07-31', '5631 Rice, Houston TX', 'F', 25000, 333445555, 5),
+    ('Ahmad', 'V', 'Jabbar', 987987987, '1969-03-29', '980 Dallas, Houston TX', 'M', 25000, 987654321, 4),
+    ('James', 'E', 'Borg', 888665555, '1937-11-10', '450 Stone, Houston TX', 'M', 55000, NULL, 1);
+select * from EMPLOYEE;
+
+
+insert into DEPARTMENT
+values
+    ('Research', 5, '333445555', '1988-05-22'),
+	('Administration', 4, '987654321', '1995-01-01'),
+    ('Headquarters', 1, '888665555', '1981-06-19');
+
+select * from DEPARTMENT;
+
+
+
+
+
+
+-- Insert Data end 
+
+
+
+
+
+
+
